83882f1cb56def640a556aa06d858ee31c6ffa9f
brook.yang <yangtaohnmiao8@126.com>
2021-07-19 13:15:53 +0800

    EService client commit

--- src/main/java/com/jiahui/esaasclient/esaasclient/controller/esaasdemoController.java
+++ src/main/java/com/jiahui/esaasclient/esaasclient/controller/esaasdemoController.java
@@ -1,5 +1,7 @@
 package com.jiahui.esaasclient.esaasclient.controller;
 
+import org.apache.skywalking.apm.toolkit.trace.ActiveSpan;
+import org.apache.skywalking.apm.toolkit.trace.TraceContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,5 +16,15 @@ public class esaasdemoController {
     @GetMapping("/jiahuiconsumer/test")
     public String testController(){
         return nacosFeignw.nacosTestDemo();
+    }
+
+
+    @GetMapping("/jiahuiconsumer/getTrace")
+    public  String providerGetTrace()
+    {
+        ActiveSpan.error(new RuntimeException("Test-ERROR-Throwable"));
+        ActiveSpan.info("Test-Info-Msg");
+        ActiveSpan.debug("Test-Debug-Msg");
+        return TraceContext.traceId();
     }
 }
